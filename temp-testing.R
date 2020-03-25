059ef1ccda5ea429eb9781b79e52eb191f2124fb
Samuel Jenness <smjenness@gmail.com>
2020-03-25 14:44:43 -0400

    Create temp-testing.R

---
+++ temp-testing.R
@@ -0,0 +1,9 @@
+
+# Testing
+# dat <- init_covid(est, param, init, control, s = 1)
+# for (at in 2:250) {
+#   dat <- resim_nets_covid(dat, at)
+#   dat <- infect_covid(dat, at)
+#   dat <- progress_covid(dat, at)
+#   dat <- prevalence_covid(dat, at)
+# }
